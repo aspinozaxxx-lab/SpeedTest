a5118740afeaa3a30c55f286a637661f2ccc8d16
aspinozaxxx-lab <aspinoza.xxx@gmail.com>
2025-11-19 13:04:22 +0300

    all work

---
+++ run_incoming_tasks.bat
@@ -0,0 +1,3 @@
+﻿@echo off
+REM Zapusk nagruzki dlya filtra Incoming Tasks cherez JMeter CLI
+"C:\tools\jmeter\bin\jmeter.bat" -n -t "C:\Projects\Jmetr\SpeedTest\tests\IncomingTasksFilter.jmx" -l "C:\Projects\Jmetr\SpeedTest\tests\incoming_tasks.jtl"
---
+++ run_td_registry.bat
@@ -0,0 +1,3 @@
+﻿@echo off
+REM Zapusk nagruzki dlya filtra Reestra TD cherez JMeter CLI
+"C:\tools\jmeter\bin\jmeter.bat" -n -t "C:\Projects\Jmetr\SpeedTest\tests\TDRegistryFilter.jmx" -l "C:\Projects\Jmetr\SpeedTest\tests\td_registry.jtl"
